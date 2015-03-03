443eeafbeb97ad664215fd92ee98563d11863148
0x2f8f <0x2f8f@gmail.com>
2015-03-03 15:52:23 +0500

    languages in translation behavior

--- Behavior/ExtendedSluggableBehavior.php
+++ Behavior/ExtendedSluggableBehavior.php
@@ -138,7 +138,48 @@ if (\$this->isColumnModified($const) && \$this->{$this->getColumnGetter()}()) {
         $this->addLimitSlugSize($script);
         $this->addMakeSlugUnique($script);
 
+        $this->sortI18ns($script);
+
         return $script;
+    }
+
+    /**
+     * Метож сортировки элементов языковых версий
+     *
+     * @param $script
+     */
+    protected function sortI18ns(&$script)
+    {
+        $kernel = new \AppKernel('prod', false);
+        $kernel->boot();
+        $locales = $kernel->getContainer()->getParameter("locales");
+        $langs = "array(";
+        foreach ($locales as $locale) {
+            $langs.='"'.$locale.'",';
+        }
+        $langs.=')';
+
+        $script .= '
+/**
+ * Сортирует элементы массива по порядку следования языков
+ *
+ * @param $elements
+ * @return array
+ */
+protected function sortI18ns($elements) {
+    if (count($elements)) {
+        $result = new \PropelObjectCollection();
+        $langs = array_flip('.$langs.');
+        foreach($elements as $element) {
+            $result[$langs[$element->getLocale()]] = $element;
+        }
+        $result->ksort();
+        return $result;
+    } else {
+        return $elements;
+    }
+}
+    ';
     }
 
     protected function addSlugSetter(&$script)
@@ -456,14 +497,36 @@ public function findOneBySlug(\$slug, \$con = null)
      */
     public function objectFilter(&$script)
     {
-        $i18n_languages = $this->getParameter('i18n_languages');
+        $to_string_method = $this->getToStringMethod();
+        $get_i18ns_method = $this->getI18nsMethod();
+        $i18ns_method_name = 'get'.$this->CamelCase($this->getTable()->getName()).'I18ns';
+
+        $table = $this->getTable();
+        $newToStringMethod = sprintf($to_string_method, $table->getName(), $table->getPhpName(), $table->getPhpName());
+        $newI18nMethod = sprintf($get_i18ns_method, $table->getName(), $table->getPhpName(), $table->getPhpName());
+
+        $parser = new \PropelPHPParser($script, true);
+        $parser->replaceMethod('__toString', $newToStringMethod);
+        $parser->replaceMethod($i18ns_method_name, $newI18nMethod);
+        $script = $parser->getCode();
+    }
+
+    /**
+     * Переопределение метода _toString
+     *
+     * @return string
+     * @throws \Exception
+     */
+    protected function getToStringMethod()
+    {
         $primary_string = $this->getParameter('primary_string');
+        $i18n_languages = $this->getParameter('i18n_languages');
         $primary_string_column =  $i18n_languages!='false' ? $primary_string : $this->getColumnForParameter('primary_string');
+        $get_primary_string = 'get'.($i18n_languages!='false' ? $this->CamelCase($primary_string) : $primary_string_column->getPhpName());
+
         if(!$primary_string_column) {
             throw new \Exception('<------ERROR------- Not found column "'.$primary_string.'" in table '.$this->getTable()->getName().' ------ERROR------->');
         }
-
-        $get_primary_string = 'get'.($i18n_languages!='false' ? $this->CamelCase($primary_string) : $primary_string_column->getPhpName());
 
         $toString = '
 
@@ -493,14 +556,80 @@ public function findOneBySlug(\$slug, \$con = null)
         }
         $toString .= '
     }
+    ';
+        return $toString;
+    }
+
+    /**
+     * Перелпределение метода getI18ns
+     *
+     * @return string
+     */
+    protected function getI18nsMethod()
+    {
+        $class_name = $this->CamelCase($this->getTable()->getName());
+        $get_i18ns_method = '
+    /**
+     * Gets an array of FaqQuestionGroupI18n objects which contain a foreign key that references this object.
+     *
+     * If the $criteria is not null, it is used to always fetch the results from the database.
+     * Otherwise the results are fetched from the database the first time, then cached.
+     * Next time the same method is called without $criteria, the cached collection is returned.
+     * If this FaqQuestionGroup is new, it will return
+     * an empty collection or the current collection; the criteria is ignored on a new object.
+     *
+     * @param Criteria $criteria optional Criteria object to narrow the query
+     * @param PropelPDO $con optional connection object
+     * @return PropelObjectCollection|'.$class_name.'I18n[] List of '.$class_name.'I18n objects
+     * @throws PropelException
+     */
+    public function get'.$class_name.'I18ns($criteria = null, PropelPDO $con = null)
+    {
+        $partial = $this->coll'.$class_name.'I18nsPartial && !$this->isNew();
+        if (null === $this->coll'.$class_name.'I18ns || null !== $criteria  || $partial) {
+            if ($this->isNew() && null === $this->coll'.$class_name.'I18ns) {
+                // return empty collections
+                $this->init'.$class_name.'I18ns();
+            } else {
+                $coll'.$class_name.'I18ns = '.$class_name.'I18nQuery::create(null, $criteria)
+                    ->filterBy'.$class_name.'($this)
+                    ->find($con);
+                $coll'.$class_name.'I18ns = $this->sortI18ns($coll'.$class_name.'I18ns);
+                if (null !== $criteria) {
+                    if (false !== $this->coll'.$class_name.'I18nsPartial && count($coll'.$class_name.'I18ns)) {
+                      $this->init'.$class_name.'I18ns(false);
+
+                      foreach ($coll'.$class_name.'I18ns as $obj) {
+                        if (false == $this->coll'.$class_name.'I18ns->contains($obj)) {
+                          $this->coll'.$class_name.'I18ns->append($obj);
+                        }
+                      }
+
+                      $this->coll'.$class_name.'I18nsPartial = true;
+                    }
+
+                    $coll'.$class_name.'I18ns->getInternalIterator()->rewind();
+
+                    return $coll'.$class_name.'I18ns;
+                }
+
+                if ($partial && $this->coll'.$class_name.'I18ns) {
+                    foreach ($this->coll'.$class_name.'I18ns as $obj) {
+                        if ($obj->isNew()) {
+                            $coll'.$class_name.'I18ns[] = $obj;
+                        }
+                    }
+                }
+
+                $this->coll'.$class_name.'I18ns = $coll'.$class_name.'I18ns;
+                $this->coll'.$class_name.'I18nsPartial = false;
+            }
+        }
+
+        return $this->coll'.$class_name.'I18ns;
+    }
         ';
-
-        $table = $this->getTable();
-        $newToStringMethod = sprintf($toString, $table->getName(), $table->getPhpName(), $table->getPhpName());
-
-        $parser = new \PropelPHPParser($script, true);
-        $parser->replaceMethod('__toString', $newToStringMethod);
-        $script = $parser->getCode();
+        return $get_i18ns_method;
     }
 
     /**
